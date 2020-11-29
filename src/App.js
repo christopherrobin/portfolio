cde4212a098d5d9ca66dafc91fe0ceed4648c00d
christopherrobin <christopherrobinreynolds@gmail.com>
2020-11-28 19:04:53 -0500

    Rough draft for loading screen and main app view / reacstrap implementation / profile pic

--- src/App.js
+++ src/App.js
@@ -1,25 +1,72 @@
 import React, { useState, useEffect } from 'react';
-import { Spinner } from 'reactstrap';
+import { Container, Row, Col, Spinner } from 'reactstrap';
 import './App.css';
 
+import GitHubIcon from '@material-ui/icons/GitHub';
+import LinkedInIcon from '@material-ui/icons/LinkedIn';
+import MenuIcon from '@material-ui/icons/Menu';
+
+import ProfilePicture from './profilepic.png';
+
 const App = () => {
-  const [hasLoaded, setLoaded] = useState(false);
+  const [isLoading, setLoading] = useState(true);
 
   useEffect(() => {
-    setLoaded(true);
+    setLoading(false);
   }, []);
 
   return (
-    <div className="App">
-      <div id="pre-loader">
-        <Spinner color="info" style={{ width: '3em', height: '3em' }} />
-        <p className="sub-title">christopher robin reynolds</p>
+    <Container id="app-container">
+      <div className="App">
+        {
+          isLoading ?
+            <div id="pre-loader">
+              <Row>
+                <Col xs={12}>
+                  <Spinner color="info" id="spinner" />
+                  <p className="sub-title">Christopher Robin Reynolds</p>
+                  <p>.com</p>
+                </Col>
+              </Row>
+            </div>
+            :
+            <div id="loaded">
+              <div id="top-banner">
+              <Row>
+                <Col xs={2}>
+                  <MenuIcon />
+                </Col>
+                <Col xs={8}>
+                  <a
+                    alt=""
+                    href="http://github.com/christopherrobin"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    >
+                    <GitHubIcon />
+                    </a>
+                  <a
+                    alt=""
+                    href="https://www.linkedin.com/in/christopherrr"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    style={{ marginLeft: '1em' }}
+                  >
+                    <LinkedInIcon />
+                  </a>
+                </Col>
+              </Row>
+              </div>
+              <Row>
+                <Col xs={12}>
+                  <img id="profilePicture" alt="Profile" src={ProfilePicture} />
+                  <h1 className="sub-title">Christopher Robin Reynolds</h1>
+                </Col>
+              </Row>
+            </div>
+        }
       </div>
-      <div id="loaded">
-        <h1>Content</h1>
-        hasLoaded: {hasLoaded ? 'true' : 'false' }
-      </div>
-    </div>
+    </Container>
   );
 }
 
