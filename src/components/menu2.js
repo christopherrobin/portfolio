2579dcc14192e23d46bfc7a14a69dcbfffb0893e
christopherrobin <christopherrobinreynolds@gmail.com>
2021-05-28 16:31:28 -0400

    Remove project from menu as well as WIP dark mode button

--- src/components/menu2.js
+++ src/components/menu2.js
@@ -27,6 +27,7 @@ const Menu = props => {
 
   return (
     <Container id="Menu-Component--Container" className={ isDark ? 'dark-mode' : null}>
+{/*
         <Row>
             <Col xs={12}>
                 <Button
@@ -38,12 +39,13 @@ const Menu = props => {
                 <small>{isDark ? 'Dark Mode' : 'Light Mode' }</small>
             </Col>
         </Row>
+*/}
         <Row>
             <Col xs={12}>
                 <div id="Menu-Component-SubNav">
                     <NavLink to="/hello" activeClassName="selected-menu-item"><ButtonToggle color="secondary">Home</ButtonToggle></NavLink>
                     <NavLink to="/resume" activeClassName="selected-menu-item"><ButtonToggle color="secondary">Resume</ButtonToggle></NavLink>
-                    <NavLink to="/projects" activeClassName="selected-menu-item"><ButtonToggle color="secondary">Projects</ButtonToggle></NavLink>
+                    {/*<NavLink to="/projects" activeClassName="selected-menu-item"><ButtonToggle color="secondary">Projects</ButtonToggle></NavLink> */}
                     <NavLink to="/contact" activeClassName="selected-menu-item"><ButtonToggle color="secondary">Contact</ButtonToggle></NavLink>
                 </div>
             </Col>
