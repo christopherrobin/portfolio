79b8970a43ee5d0c694ef7a65d2a1ddb5de5ca78
Christopher Robin Reynolds <christopherrobinreynolds@gmail.com>
2021-06-14 15:43:56 -0400

    Let's try it from /src

--- src/components/ResumeDownloadButton.js
+++ src/components/ResumeDownloadButton.js
@@ -2,21 +2,9 @@ import React from "react";
 import Button from "@material-ui/core/Button";
 import DescriptionIcon from "@material-ui/icons/Description";
 import Tooltip from '@material-ui/core/Tooltip';
-import FileSaver from 'file-saver';
-
-import { get } from 'lodash';
+import resumePDF from '.././ChristopherReynolds_SeniorSoftwareEngineer_2021_web.pdf';
 
 const ResumeDownloadButton = () => {
-
-  console.log(get(process, 'env.PUBLIC_URL', false));
-
-  const saveFile = () => {
-    FileSaver.saveAs(
-      './ChristopherReynolds_SeniorSoftwareEngineer_2021_web.pdf',
-      "ChristopherReynolds_SeniorSoftwareEngineer.pdf"
-    );
-  };
-
   return (
     <div className="btn-resume-download">
       <Tooltip title="Download My Resume">
@@ -28,12 +16,12 @@ const ResumeDownloadButton = () => {
           startIcon={<DescriptionIcon />}
           className="external-links-button"
           alt="Download My Resume"
-          onClick={saveFile}
+          // onClick={saveFile}
         >
           Download Resume (PDF)
         </Button>
       </Tooltip>
-      <a href='/ChristopherReynolds_SeniorSoftwareEngineer.pdf'>test</a>
+      <a href={resumePDF}>test</a>
     </div>
   );
 }
